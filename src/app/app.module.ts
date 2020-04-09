60a04896b6b15ca67b06eb9258f52db20d5d1aa7
SathwikaGone <gonesathwika@gmail.com>
2020-04-09 18:25:47 -0400

    input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ProfiledetailComponent } from './profiledetail/profiledetail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProfileComponent,
-    ProfiledetailComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ProfileComponent, ProfiledetailComponent],
+  imports: [FormsModule, BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/profiledetail/profiledetail.component.ts
+++ src/app/profiledetail/profiledetail.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from '../User';
 @Component({
   selector: 'app-profiledetail',
   templateUrl: './profiledetail.component.html',
-  styleUrls: ['./profiledetail.component.css']
+  styleUrls: ['./profiledetail.component.css'],
 })
 export class ProfiledetailComponent implements OnInit {
+  // User = {
+  //   Fname: '',
+  //   Lname: '',
+  // };
 
-  constructor() { }
+  // @Input() user: User;
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
+  Fname = '';
+  Lname = '';
 }
